95beeaa7de33e73bf8b13a7bf953ce160f1a6f1e
Dominik <dominik.proficz@gmail.com>
2021-05-10 17:01:50 +0200

    frist commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import CostForm from "./CostForm/costForm"
 
+
+
 const App =()=> {
   return (
   <div>
--- src/CostForm/costForm.jsx
+++ src/CostForm/costForm.jsx
@@ -1,82 +1,181 @@
-import React from 'react';
+import { React, useState, createRef } from "react";
+import axios from 'axios';
+import { year, voivodships, stateOfConstructionOptions, workOrderLevel} from "./dataForm"
 
- const CostForm =()=> {
-    
-const voivodships = ["dolnośląskie",
-"kujawsko-pomorskie",
-"lubelskie",
-"lubuskie",
-"łódzkie",
-"małopolskie",
-"mazowieckie",
-"opolskie",
-"podkarpackie",
-"podlaskie",
-"pomorskie",
-"śląskie",
-"świętokrzyskie",
-"warmińsko-mazurskie",
-"wielkopolskie",
-"zachodniopomorskie"]
-const stateOfConstructionOptions = ["SSO - stan surowy otwart", "SSZ - stan surowy zamknięty", "DEW - stan deweloperski"]
-const workOrderLevel = ["100% - wszystkie prace zostały zlecone", "75% - prac zostało zleconych", "50% - prac zostało zleconych", "25% - prac zostało zleconych", "100% - prac wykonałem samodzielnie" ]
+const CostForm = () => {
+  const [emptyFormData, setEmptyFormData] = useState({
+    info: {
+      id: "",
+      year: "",
+      voivodeship: "",
+      workStage: "",
+      workOrderLevel: "",
+      houseArea: 0,
+      constructionCost: 0,
+    }
+  })
+  const [formData, setFormData] = useState({
+    info: {
+      id: "",
+      year: "",
+      voivodeship: "",
+      workStage: "",
+      workOrderLevel: "",
+      houseArea: 0,
+      constructionCost: 0,
+    },
+  });
 
+  const yearInput = createRef();
+  const voivodeshipInput = createRef();
+  const workStageInpiut = createRef();
+  const workLevelInput = createRef();
+  const houseAreaInput = createRef();
+  const constructionCostInput = createRef();
 
-    return(
-        <div>
-            <form>
-                    <div>
-                        <h3>Rok rozpoczecia budowy</h3>
-                        <input type="text" placeholder="Rok"></input>
-                    </div>
-                    <div>
-                        <h3>Województwo gdzie prowadzona jest inwestycja</h3>
-                            <select id = "myList">
-                                {voivodships.map(item=>{
-                                return <option value = "1">{item.toUpperCase()}</option>
-                                })}
-                            </select >
-                    </div>
-                    <div>
-                        <h3>Stan robót</h3>
-                            {stateOfConstructionOptions.map(item=>{
-                                return(
-                                <div>
-                                    <input type="checkbox" id={item}/>
-                                    <label for={item}>{item}}</label>
-                                </div>
-                                )
-                                
-                            })}
-                    </div>
-                    <div>
-                        <h3>Poziom zlecanych prac</h3>
-                            {workOrderLevel.map(workLevel => {
-                                return (
-                                <div>
-                                    <input name="workorOrd" type="radio" id={workLevel} />
-                                    <label for={workLevel} value={workLevel}>{workLevel}</label>
-                                </div>
-                                )
+  const updateForm = (e, ref) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      info: {
+        ...prevState.info,
 
-                            })}
-                    </div>
-                    <div>
-                        <h3>Całkowita powierzchnia domu - wraz z garażem i piwnicą</h3>
-                          <input type="text" placeholder="powierzchnia budynku"/> 
-                    </div>
-                
-                    <div>
-                        <h3>Całkowity koszt budowy brutto</h3>
-                         <input type="text" placeholder="kwota brutto"/> 
-                    </div>
-                
-            </form>
-            <button>Wyślij</button>
-       </div>
+        [e.target.name]: ref.current.value,
+      },
+    }));
+    console.log(formData.info);
+  };
+  const checkRadio = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      info: {
+        ...prevState.info,
+        [e.target.name]: e.target.value,
+      },
+    }));
+    console.log(formData);
+  };
 
-    )
-
+const sendToDb =()=>{
+  axios({
+    method: 'post',
+    url: 'http://localhost:3000/costs',
+    data:   formData.info,
+  })
+  .then((response)=>{
+    console.log(response)
+  }, (error)=> console.log(error)
+  )
 }
+  
+  return (
+    <>
+      <div>
+        <h3>Rok rozpoczecia budowy {formData.info.year}</h3>
+
+        <select
+          id="myData"
+          onClick={(e) => updateForm(e, yearInput)}
+          name={"year"}
+          ref={yearInput}
+        >
+          {year.map((item) => {
+            return (
+              <option key={item.year} name={"year"} value={item}>
+                {item}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+
+      <div>
+        <h3>
+          Województwo gdzie prowadzona jest inwestycja{" "}
+          {formData.info.voivodeship}
+        </h3>
+
+        <select
+          id="myList"
+          onClick={(e) => updateForm(e, voivodeshipInput)}
+          name={"voivodeship"}
+          ref={voivodeshipInput}
+        >
+          {voivodships.map((item) => {
+            return (
+              <option key={item.voivodeship} name={"voivodeship"} value={item}>
+                {item}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+
+      <div>
+        <h3>Stan robót</h3>
+
+        {stateOfConstructionOptions.map((item) => {
+          return (
+            <div>
+              <label onClick={(e) => checkRadio(e)} for={"workStage"}>
+                {item}
+                <input
+                  key={item.workStage}
+                  name={"workStage"}
+                  type="radio"
+                  value={item}
+                  ref={workStageInpiut}
+                ></input>
+              </label>
+            </div>
+          );
+        })}
+      </div>
+      <div>
+        <h3>Poziom zlecanych prac</h3>
+        {workOrderLevel.map((item) => {
+          return (
+            <div>
+              <label key={"workOrderLevel"} for={"workLevel"}>
+                {item}
+                <input
+                  onClick={(e) => checkRadio(e)}
+                  name={"workOrderLevel"}
+                  type="radio"
+                  value={item}
+                  ref={workLevelInput}
+                  key={item.workOrderLevel}
+                />
+              </label>
+            </div>
+          );
+        })}
+      </div>
+      <div>
+        <h3>Całkowita powierzchnia domu - wraz z garażem i piwnicą</h3>
+        <input
+          onChange={(e) => updateForm(e, houseAreaInput)}
+          type="number"
+          step="1"
+          placeholder="powierzchnia budynku"
+          name={"houseArea"}
+          ref={houseAreaInput}
+        />
+      </div>
+
+      <div>
+        <h3>Całkowity koszt budowy brutto</h3>
+        <input
+          onChange={(e) => updateForm(e, constructionCostInput)}
+          type="number"
+          step="1000"
+          placeholder="kwota brutto"
+          name={"constructionCost"}
+          ref={constructionCostInput}
+        />
+      </div>
+      <button onClick={() => {sendToDb()}}>Wyślij</button>
+    </>
+  );
+};
 
 export default CostForm;
